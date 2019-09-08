e48576d4511e057316cd3c4bb54c56e103d73d4e
Stan <skhalipa@gmail.com>
2019-09-08 21:14:25 +0300

    Server wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'konfect');
+define('DB_NAME', 'cf24411_0');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'cf24411_0');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Stl80XXX');
+define('DB_PASSWORD', '9QJQca8I');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
@@ -80,22 +80,6 @@ $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
-
-define('FS_METHOD', 'direct');
-
-// Enable WP_DEBUG mode
-define( 'WP_DEBUG', true );
-
-// Enable Debug logging to the /wp-content/debug.log file
-define( 'WP_DEBUG_LOG', true );
-
-// Disable display of errors and warnings
-define( 'WP_DEBUG_DISPLAY', false );
-@ini_set( 'display_errors', 0 );
-
-// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-define( 'SCRIPT_DEBUG', true );
-
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
