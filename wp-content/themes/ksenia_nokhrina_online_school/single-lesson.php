9a5b88a23d742cc52ab54d60a6b326858c9ae9f9
Stan <skhalipa@gmail.com>
2019-09-10 03:11:23 +0300

    Added relevant lessons, added video with embed

--- wp-content/themes/ksenia_nokhrina_online_school/single-lesson.php
+++ wp-content/themes/ksenia_nokhrina_online_school/single-lesson.php
@@ -15,6 +15,7 @@ $lesson_info = array(
     'media_content_description' => get_post_meta($post->ID, 'media_content_description', true),
     'parent_course'             => intval(get_post_meta($post->ID, '_llms_parent_course', true)),
     'lessons'                   => null,
+    'media_content'             => get_post_meta($post->ID, '_llms_video_embed', true),
 );
 
 switch(strtolower($lesson_info['complexity'])){
@@ -36,7 +37,6 @@ $lesson_info['lessons']->query(array(
     )
 ));
 
-$lesson_info['lessons'] = intval($lesson_info['lessons']->post_count);
 ?>
     <main class="course">
         <div class="favorites"></div>
@@ -46,10 +46,8 @@ $lesson_info['lessons'] = intval($lesson_info['lessons']->post_count);
             <span class="duration"><?php echo $lesson_info['duration']; ?> ч.</span>
             <span class="difficult" data-level="mid"><?php echo $lesson_info['complexity']; ?> сложность</span>
             <span class="price"><?php echo $lesson_info['price']; ?> РУБЛЕЙ</span>
-            <span class="num_of_lessons"><?php echo $lesson_info['lessons']; ?> УРОКОВ</span>
+            <span class="num_of_lessons"><?php echo intval($lesson_info['lessons']->post_count); ?> УРОКОВ</span>
         </div>
-
-        <?php do_shortcode('[embed width="123" height="456"]http://www.youtube.com/watch?v=dQw4w9WgXcQ[/embed]'); ?>
 
         <div class="separate_set_slider">
 
@@ -108,9 +106,14 @@ $lesson_info['lessons'] = intval($lesson_info['lessons']->post_count);
                     <div class="col">
                         <div class="preview">
                             <?php
+                                echo '<div class="llms-video-wrapper">';
+                                echo '<div class="center-video">';
+
+                                $lesson = new LLMS_Lesson( $post );
+                                echo $lesson->get_video();
                                 global $wp_embed;
-                                $video_url = 'https://www.youtube.com/watch?v=b7mixrO2lzA';
-                                echo $wp_embed->run_shortcode( '[embed content_width="100%" content_height="100%" style="top:0;"]' . $video_url . '[/embed]' );
+                                echo $wp_embed->run_shortcode( '[embed]' . $lesson_info['media_content']. '[/embed]' );
+                                echo '</div></div>';
                             ?>
                         </div>
                         <button class="accordion_but">КУПИТЬ/СМОТРЕТЬ</button>
@@ -127,72 +130,36 @@ $lesson_info['lessons'] = intval($lesson_info['lessons']->post_count);
         </div><!-- /.accordion_block -->
 
         <h2>Похожие уроки</h2>
-        <div class="row lesson_row">
-            <div class="lesson">
-                <div class="col">
-                    <?php
-                    $image = get_field('изображение_урока_1');
-                    if( !empty($image) ): ?>
-                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                    <?php endif; ?>
-                    <div class="row">
-                        <span class="duration"><?php the_field('продолжительность_урока'); ?></span>
-                        <div class="complexity"></div>
-                    </div>
-                    <p><?php the_field('длинный_заголовок_одного_из_уроков'); ?></p>
-                    <a href="#" class="more">Узнать подробнее</a>
-                </div>
-            </div>
-
-            <div class="lesson">
-                <div class="col">
-                    <?php
-                    $image = get_field('изображение_урока_2');
-                    if( !empty($image) ): ?>
-                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                    <?php endif; ?>
-                    <div class="row">
-                        <span class="duration"><?php the_field('продолжительность_урока'); ?></span>
-                        <div class="complexity"></div>
-                    </div>
-                    <p><?php the_field('длинный_заголовок_урока'); ?></p>
-                    <a href="#" class="more">Узнать подробнее</a>
-                </div>
-            </div>
-
-            <div class="lesson">
-                <div class="col">
-                    <?php
-                    $image = get_field('изображение_урока_3');
-                    if( !empty($image) ): ?>
-                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                    <?php endif; ?>
-                    <div class="row">
-                        <span class="duration"><?php the_field('продолжительность_урока'); ?></span>
-                        <div class="complexity"></div>
-                    </div>
-                    <p><?php the_field('длинный_заголовок_урока'); ?></p>
-                    <a href="#" class="more">Узнать подробнее</a>
-                </div>
-            </div>
-
-            <div class="lesson">
-                <div class="col">
-                    <?php
-                    $image = get_field('изображение_урока_4');
-                    if( !empty($image) ): ?>
-                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                    <?php endif; ?>
-                    <div class="row">
-                        <span class="duration"><?php the_field('продолжительность_урока'); ?></span>
-                        <div class="complexity"></div>
-                    </div>
-                    <p><?php the_field('длинный_заголовок_урока'); ?></p>
-                    <a href="#" class="more">Узнать подробнее</a>
-                </div>
-            </div>
-
-        </div><!-- /.row -->
+        <?php
+            echo '<div class="row lesson_row">';
+            $lesson_more_count = 0;
+            foreach( $lesson_info['lessons']->posts as $p ) {
+                if( $lesson_more_count >= 4 ) break;
+                echo '<div class="lesson">';
+                    echo '<div class="col">';
+                        $image = array(
+                            'url'=> get_the_post_thumbnail_url($p, array(300,300)),
+                            'alt'=> get_the_post_thumbnail_caption($p),
+                        );
+                        echo '<img src="'.$image['url'].'" alt="'.$image['alt'].'" />';
+                        echo '<div class="row">';
+                            echo '<span class="duration">'.intval(get_post_meta($p->ID, 'duration', true)).' ч.</span>';
+                            $c=strtolower(get_post_meta($p->ID, 'complexity', true));
+                            switch(mb_strtolower($c)){
+                                case 'средняя':     $c='mid';  break;
+                                case 'повышенная':  $c='hard'; break;
+                                //default:            $complexity='';     break;
+                            }
+                            echo '<div class="complexity" data-level="'.$c.'"></div>';
+                        echo '</div>';
+                    echo '<p>'.$p->post_title.'</p>';
+                    echo '<a href="'.get_post_permalink($p->ID).'" class="more">Узнать подробнее</a>';
+                    echo '</div>';
+                echo '</div>';
+                $lesson_more_count++;
+            }
+            echo '</div>';
+        ?>
 
         <div class="partners_slider">
             <div>Баннер про кулинарную школу <br>
